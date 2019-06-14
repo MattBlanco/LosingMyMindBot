d5a4fa409d257ce8bc3d7fb164b40c9b5febc2e6
MattBlanco <matthew.e.blanco@gmail.com>
2019-06-14 13:20:47 -0500

    Wrapped run bot code in try/catch block. Moved log in back to loop

--- bot.py
+++ bot.py
@@ -19,48 +19,51 @@ def bot_login():
         
 def run_bot(bot):
     print ("\nFetching comments..")
-    # Grab all the Recent Comments in every subreddit. This will return 100 of the newest comments on Reddit
-    for results in bot.subreddit('all').comments():
-        global previous_id  # Import the global variable
+    try:
+        # Grab all the Recent Comments in every subreddit. This will return 100 of the newest comments on Reddit
+        for results in bot.subreddit('all').comments():
+            global previous_id  # Import the global variable
 
-        body = results.body  # Grab the Comment
-        body = body.lower() # Convert the comment to lowercase so we can search it no matter how it was written
-        comment_id = results.id  # Get the Comment ID
+            body = results.body  # Grab the Comment
+            body = body.lower() # Convert the comment to lowercase so we can search it no matter how it was written
+            comment_id = results.id  # Get the Comment ID
 
-        if comment_id == previous_id:  # Check if we already replied to this comment
-            return "Error"
+            if comment_id == previous_id:  # Check if we already replied to this comment
+                return "Error"
 
-        found = body.find('loosing')  # Search for loosing.
+            found = body.find('loosing')  # Search for loosing.
 
-        if found != -1:
-            previous_id = comment_id  # Add to our global variable
+            if found != -1:
+                previous_id = comment_id  # Add to our global variable
 
-            try:
-                # Reply to the Comment
-                print ("\n\nFound a comment!")
-                comment_reply = "Noticed you used the word *loosing*. Did you mean **losing?**"
-                comment_reply += "\n\n\n\n---\n\n^(Beep boop. I am a bot. If there are any issues, contact my) [^Master ](https://www.reddit.com/message/compose/?to=PositivePlayer1&subject=/u/LosingMyMindBot)\n\n"
-                results.reply(comment_reply)
-            except:
-                # Probably low karma so can't comment as frequently
-                time_remaining = 15
-                if (str(e).split()[0] == "RATELIMIT:"):
-                    for i in str(e).split():
-                        if (i.isdigit()):
-                            time_remaining = int(i)
-                            break
-                    if (not "seconds" or not "second" in str(e).split()):
-                        time_remaining *= 60
-                print (str(e.__class__.__name__) + ": " + str(e))
-                for i in range(time_remaining, 0, -5):
-                    print ("Retrying in", i, "seconds..")
-                    time.sleep(5)	
-                break
+                try:
+                    # Reply to the Comment
+                    print ("\n\nFound a comment with loosing!")
+                    comment_reply = "Noticed you used the word *loosing*. Did you mean **losing?**"
+                    comment_reply += "\n\n\n\n---\n\n^(Beep boop. I am a bot. If there are any issues, contact my) [^Master ](https://www.reddit.com/message/compose/?to=PositivePlayer1&subject=/u/LosingMyMindBot)\n\n"
+                    results.reply(comment_reply)
+                except Exception as e:
+                    # Probably low karma so can't comment as frequently
+                    time_remaining = 15
+                    if (str(e).split()[0] == "RATELIMIT:"):
+                        for i in str(e).split():
+                            if (i.isdigit()):
+                                time_remaining = int(i)
+                                break
+                        if (not "seconds" or not "second" in str(e).split()):
+                            time_remaining *= 60
+                    print (str(e.__class__.__name__) + ": " + str(e))
+                    for i in range(time_remaining, 0, -5):
+                        print ("Retrying in", i, "seconds..")
+                        time.sleep(5)	
+                    break
+    except Exception as e:
+        print (str(e.__class__.__name__) + ": " + str(e))
 
 if __name__ == "__main__":
-    reddit_bot = bot_login()
     while True:
         try:
+            reddit_bot = bot_login()
             run_bot(reddit_bot)
             time.sleep(10)
 
